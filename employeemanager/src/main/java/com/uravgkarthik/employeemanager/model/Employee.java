b399a98b89a8aad7ad65f5655af1b50a9d3dbf5b
uravgkarthik <br.karthik1@gmail.com>
2021-12-23 11:32:33 +0530

    Initial Commit

---
+++ employeemanager/src/main/java/com/uravgkarthik/employeemanager/model/Employee.java
@@ -0,0 +1,14 @@
+package com.uravgkarthik.employeemanager.model;
+
+import java.io.Serializable;
+
+@Enti
+public class Employee implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+}
